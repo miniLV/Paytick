900cd11126a47a5f29961a9a840fdf1f150b8f64
miniLV <liangyuhangApple@gmail.com>
2025-12-26 10:32:29 +0800

    test: Add unit and UI test targets
    - Set up PaytickTests for unit testing
    - Set up PaytickUITests for UI testing
    - Configure test launch arguments

---
+++ Paytick/PaytickTests/PaytickTests.swift
@@ -0,0 +1,16 @@
+//
+//  PaytickTests.swift
+//  PaytickTests
+//
+//  Created by miniLV on 2024/9/25.
+//
+
+import Testing
+
+struct PaytickTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
